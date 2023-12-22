ce2afc5d2b5b1903d4aed405eaf47d4c98e95b36
Rubel Mahmud <vxlrubel@gmail.com>
2023-12-22 23:32:01 +0600

    create form for add new user from dashboard

--- inc/admin/class-get-visitor-tamplate.php
+++ inc/admin/class-get-visitor-tamplate.php
@@ -31,9 +31,35 @@ class Get_Visitor_Template{
      */
     public function add_new_visitor(){
         ?>
-        <div class="wrap">
-            hello world
-        </div>
+            <div class="wrap">
+                <h1 class="wp-heading-inline">Add New Visitor</h1>
+                <a href="javascript:void(0)" class="page-title-action">Visitor list</a>
+                <hr class="wp-header-end">
+                <div class="notice notice-success is-dismissible"> 
+                    <p><strong>Email add successfully.</strong></p>
+                    <button type="button" class="notice-dismiss">
+                        <span class="screen-reader-text">Dismiss this notice.</span>
+                    </button>
+                </div>
+                <form action="javascript:void(0)" novalidate="novalidate">
+                    <table class="form-table" role="presentation">
+                        <tbody>
+                            <tr>
+                                <th scope="row">
+                                    <label for="email-address">Email Address</label>
+                                </th>
+                                <td>
+                                    <input name="blogname" type="email" class="regular-text" id="email-address">
+                                    <p class="submit">
+                                        <input type="submit" name="submit" class="button button-primary" value="Add User">
+                                    </p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </form>
+                
+            </div>
         
         <?php
     }
