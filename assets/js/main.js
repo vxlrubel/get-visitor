066dce952444c115541319eeffc9f875b2d700b9
Rubel Mahmud <vxlrubel@gmail.com>
2023-12-25 01:52:37 +0600

    update insert script

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,38 +8,41 @@
         }
 
         submitForm(){
-            form.on('submit', (e)=>{
-                e.preventDefault();
-                const email = form.find('input[type="email"]').val();
-                const apiUrl = GV.api_url;
-                const data = {
-                    email : email
-                }
-                const nonce = GV.nonce;
-                const headers = {
-                    'Content-Type': 'application/json',
-                    'X-WP-Nonce': nonce
-                };
+            form.each(function(){
 
-                $.ajax({
-                    type      : 'POST',
-                    url       : apiUrl,
-                    data      : JSON.stringify(data),
-                    headers   : headers,
-                    beforeSend: ()=>{
-                        form.find('.submit-parent').append('<span class="spin"></span>');
-                    },
-                    success   : ( res )=>{
-                        form.find('span.spin').remove();
-                        form.after(`<div class="notice success">${res}<span class="close">&#10006;</span></div>`);
-                        form.find('input[type="email"]').val('');
-                    },
-                    error     : ()=>{
-                        form.find('span.spin').remove();
-                        form.after('<div class="notice warning">Something went wront. <span class="close">&#10006;</span></div>')
+                $(this).on('submit', function(e){
+                    e.preventDefault();
+                    let parent = $(this).closest('.gv-collect-form');
+                    const apiUrl = GV.api_url;
+                    const email = $(this).find('input[type="email"]').val();
+                    const data = {
+                        email : email
                     }
-                });
+                    const nonce = GV.nonce;
+                    const headers = {
+                        'Content-Type': 'application/json',
+                        'X-WP-Nonce': nonce
+                    };
 
+                    $.ajax({
+                        type      : 'POST',
+                        url       : apiUrl,
+                        data      : JSON.stringify(data),
+                        headers   : headers,
+                        beforeSend: ()=>{
+                            $(this).find('.submit-parent').append('<span class="spin"></span>');
+                        },
+                        success   : ( res )=>{
+                            parent.find('span.spin').remove();
+                            $(this).after(`<div class="notice success">${res}<span class="close">&#10006;</span></div>`);
+                            parent.find('input[type="email"]').val('');
+                        },
+                        error     : ()=>{
+                            parent.find('span.spin').remove();
+                            $(this).after('<div class="notice warning">Something went wront. <span class="close">&#10006;</span></div>')
+                        }
+                    });
+                });
             });
 
             // destroy notice
