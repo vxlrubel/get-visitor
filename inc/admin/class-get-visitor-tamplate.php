4102545e91023dcadc6249075c89ce39c1e3662c
Rubel Mahmud <vxlrubel@gmail.com>
2023-12-28 23:09:41 +0600

    include the general settings options in template file

--- inc/admin/class-get-visitor-tamplate.php
+++ inc/admin/class-get-visitor-tamplate.php
@@ -106,65 +106,9 @@ class Get_Visitor_Template{
                             <li><a href="#options">Options</a></li>
                         </ul>
 
-                        <div id="general">
-                            <form action="javascript:void();" class="settings-general-form">
-                                <table class="form-table" role="presentation">
-                                    <tbody>
-                                        <tr>
-                                            <th scope="row">
-                                                <label for="title">Form Title</label>
-                                                <span class="small">Show the title in the frontend.</span>
-                                            </th>
-                                            <td>
-                                                <input type="text" class="regular-text" id="title">
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">
-                                                <label for="desc">Description</label>
-                                                <span class="small">Insert short description.</span>
-                                            </th>
-                                            <td>
-                                                <textarea id="desc" class="regular-text" rows="3"></textarea>
-                                                <span class="small">This descriptions show above the subscribe form.</span>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">
-                                                <label for="placeholder">Placeholder</label>
-                                                <span class="small">Change placeholder text from here.</span>
-                                            </th>
-                                            <td>
-                                                <input type="text" class="regular-text" id="placeholder">
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">
-                                                <label for="success">Success</label>
-                                                <span class="small">Change success notice.</span>
-                                            </th>
-                                            <td>
-                                                <input type="text" class="regular-text" id="success">
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">
-                                                <label for="warning">Warning</label>
-                                                <span class="small">Change warning notice.</span>
-                                            </th>
-                                            <td>
-                                                <input type="text" class="regular-text" id="warning">
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-
-                                <p class="submit">
-                                    <input type="submit" class="button button-primary" value="Save Changes">
-                                </p>
-
-                            </form>
-                        </div>
+                        <?php
+                            require_once dirname(__FILE__) . '/options/general.php';
+                        ?>
 
                         <div id="options">
                             <form action="javascript:void();" class="settings-option-form">
