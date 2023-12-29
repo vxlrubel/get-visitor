0d02ca86a652d2b5a3ee60a038f49bb1b509219d
Rubel Mahmud <vxlrubel@gmail.com>
2023-12-30 01:17:34 +0600

    create how to use options tab

---
+++ inc/admin/options/how-to-use.php
@@ -0,0 +1,22 @@
+<?php
+// directly access denied
+defined('ABSPATH') || exit;
+?>
+<div id="how-to-use">
+    <table class="form-table" role="presentation">
+        <tbody>
+            <tr>
+                <th scope="row">
+                    <label for="item-count">Shortcode</label>
+                </th>
+                <td>
+                    <div class="shortcode">
+                        <span class="get-shortcode">[get_visitor_form]</span>
+                        <span class="copy-shortcode">Copy</span>
+                    </div>
+                    <p>Click the copy button and paste it where you want to show the form.</p>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
