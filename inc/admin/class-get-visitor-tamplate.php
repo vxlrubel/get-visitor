8de51feb5faf6faad8d3624aee16c89418648ba6
Rubel Mahmud <vxlrubel@gmail.com>
2023-12-30 00:49:50 +0600

    include options file

--- inc/admin/class-get-visitor-tamplate.php
+++ inc/admin/class-get-visitor-tamplate.php
@@ -107,46 +107,11 @@ class Get_Visitor_Template{
                         </ul>
 
                         <?php
+                        
                             require_once dirname(__FILE__) . '/options/general.php';
+
+                            require_once dirname(__FILE__) . '/options/options.php';
                         ?>
-
-                        <div id="options">
-                            <form action="javascript:void();" class="settings-option-form">
-                                <table class="form-table" role="presentation">
-                                    <tbody>
-                                        <tr>
-                                            <th scope="row">
-                                                <label for="item-count">Items</label>
-                                                <span class="small">Show items in the table data.</span>
-                                            </th>
-                                            <td>
-                                                <select name="" id="item-count" class="regular-text">
-                                                    <option value="10">10</option>
-                                                    <option value="15">15</option>
-                                                    <option value="20">20</option>
-                                                    <option value="25">25</option>
-                                                    <option value="30">30</option>
-                                                </select>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">
-                                                <label for="desc">Description</label>
-                                                <span class="small">Insert short description.</span>
-                                            </th>
-                                            <td>
-                                                <textarea id="desc" class="regular-text" rows="3"></textarea>
-                                                <span class="small">This descriptions show above the subscribe form.</span>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-
-                                <p class="submit">
-                                    <input type="submit" class="button button-primary" value="Save Changes">
-                                </p>
-                            </form>
-                        </div>
                     </div>
                 </div>
 
