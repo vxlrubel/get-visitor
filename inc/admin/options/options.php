593b27ec31bca64097c145778513d4c7a1b927ca
Rubel Mahmud <vxlrubel@gmail.com>
2023-12-31 02:14:12 +0600

    update select option

--- inc/admin/options/options.php
+++ inc/admin/options/options.php
@@ -12,13 +12,13 @@ defined('ABSPATH') || exit;
                         <span class="small">Show items in the table data.</span>
                     </th>
                     <td>
-                        <?php $items = (int)get_option('_gv_list_items'); ?>
+                        <?php $items = get_option('_gv_list_items'); ?>
                         <select id="item-count" class="regular-text">
                             <option value="10" <?php echo selected( $items, 10 );?>>10</option>
                             <option value="20" <?php echo selected( $items, 20 );?>>20</option>
                             <option value="30" <?php echo selected( $items, 30 );?>>30</option>
                             <option value="40" <?php echo selected( $items, 40 );?>>40</option>
-                            <option value="50" <?php echo selected( $items, 30 );?>>50</option>
+                            <option value="50" <?php echo selected( $items, 50 );?>>50</option>
                         </select>
                     </td>
                 </tr>
