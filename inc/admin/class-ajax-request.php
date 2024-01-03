3a43613ff861837f197cde0d635854dd6e3b36e8
Rubel Mahmud <vxlrubel@gmail.com>
2024-01-03 22:57:50 +0600

    update ajax request for export data

--- inc/admin/class-ajax-request.php
+++ inc/admin/class-ajax-request.php
@@ -10,8 +10,45 @@ use GetVisitor\Get_Visitor_Table as Database_Table;
 class Ajax_Request{
     use Database_Table;
 
-    public function __construct(){
-
-    }
+    // export action
+    private $action_export = 'export_get_visitors';
     
+    public function __construct(){
+        add_action( "wp_ajax_{$this->action_export}", [ $this, 'export_data' ] );
+        add_action( "wp_ajax_nopriv_{$this->action_export}", [ $this, 'export_data' ] );
+    }
+
+    /**
+     * export visitor list
+     *
+     * @return void
+     */
+    public function export_data(){
+        global $wpdb;
+        $table = $this->table();
+
+        if( ! defined('DOING_AJAX') && ! DOING_AJAX ){
+            return;
+        }
+
+        if ( ! isset( $_POST['action']) && $_POST['action'] !== 'export_get_visitors' ){
+            return;
+        }
+
+        $csv_data = "Email Address, Created Date, Updated Date \n";
+        $sql      = "SELECT * FROM $table";
+        $rows = $wpdb->get_results( $sql );
+
+        foreach ( $rows as $row ) {
+            $csv_data .= "{$row->email}, {$row->created_at}, {$row->updated_at}\n";
+        }
+        $filename = 'get-visitors_'. rand( 9999, 999999 ) . '.csv';
+        header( 'Content-Type: text/csv' );
+        header( 'Content-Disposition: attachment; filename="' . $filename . '"' );
+
+        echo $csv_data;
+        
+        wp_die();
+    }
+
 }
