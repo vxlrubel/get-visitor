aa5f1d27c25130b784509885e5d1b2fb42013084
Rubel Mahmud <vxlrubel@gmail.com>
2024-01-03 22:26:09 +0600

    create a class for handle ajax request

---
+++ inc/admin/class-ajax-request.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace GetVisitor\Admin;
+
+// derectly access denied
+defined('ABSPATH') || exit;
+
+use GetVisitor\Get_Visitor_Table as Database_Table;
+
+class Ajax_Request{
+    use Database_Table;
+
+    public function __construct(){
+
+    }
+    
+}
